cd2cf4d2883c20222970b034777efca98dbbbca1
root <vladb95z-soft@yandex.ru>
2016-06-03 15:57:00 +0300

    inited a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp      = require('gulp'),
+    concatJs  = require('gulp-concat'),
+    jade      = require('gulp-jade'),
+    concatCss = require('gulp-concat-css');
+
+
+gulp.task('css-build', function(){
+  gulp.src('/css/*.css')
+      .pipe(concatCss('bundle.styles.css'))
+      .pipe(gulp.dest('bundle'));
+});
+
+gulp.task('jade-templates-build', function(){
+  gulp.src(['/templates/jade/*.jade', '!/templates/jade/index.jade'])
+      .pipe(jade({
+        clients: true,
+        pretty:  true
+      }))
+      .pipe(gulp.dest('templates'));
+});
+
+gulp.task('jade-index-build', function(){
+  gulp.src('/templates/jade/index.jade')
+      .pipe(jade({
+        clients: true,
+        pretty:  true
+      }))
+      .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['css-build', 'jade-templates-build', 'jade-index-build']);
