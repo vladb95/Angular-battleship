173f031f054f370f133003552d5bb1a00ad41a75
root <vladb95z-soft@yandex.ru>
2016-06-03 22:50:59 +0300

    create task for build requirences file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
-var gulp      = require('gulp'),
-    concatJs  = require('gulp-concat'),
-    jade      = require('gulp-jade'),
-    concatCss = require('gulp-concat-css'),
+var gulp                = require('gulp'),
+    concatJs            = require('gulp-concat'),
+    jade                = require('gulp-jade'),
+    concatCss           = require('gulp-concat-css'),
     typeScriptCompiler  = require('gulp-tsc');
 
+var requirences = {
+  js:  [
+    './bower_components/angular.min.js',
+    './bower_components/jquery/dist/jquery.min.js'
+  ],
+  css: [
+    './bower_components/bootstrap/dist/css/bootstrap.min.css'
+  ]
+};
+
+gulp.task('build-requirences', function(){
+  gulp.src(requirences.js)
+      .pipe(concatJs('bundle.requirences.js'))
+      .pipe(gulp.dest('bundle'));
+  gulp.src(requirences.css)
+      .pipe(concatCss('bundle.requirences.css'))
+      .pipe(gulp.dest('bundle'));
+})
 
 gulp.task('css-build', function(){
   gulp.src('./css/*.css')
