4deadf787c613de78356086ad2aa9797c093696b
vladb95_ <vladb95z-soft@yandex.ru>
2016-06-20 12:01:51 +0300

    added new router

--- app/config.ts
+++ app/config.ts
@@ -2,14 +2,21 @@
 
 module Application.Config{
   export class RouteConfig{
-    constructor($stateProvider: ng.ui.IStateProvider){
+    constructor($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider){
+
       $stateProvider
         .state('index', {
-          url: "",
+          url: "/",
           templateUrl: "templates/main.html",
           controller: "MainPageController",
           controllerAs: "vm"
         })
+        .state('index.constructor', {
+          url: '/constructor',
+          templateUrl: "templates/constructor.html",
+        });
+        $urlRouterProvider
+          .otherwise('/');
     }
   }
 }
--- app/module.ts
+++ app/module.ts
@@ -1,7 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-angular.module('battle-ship-app', ['ui.router']);
+angular.module('battle-ship-app', ['ngRoute']);
+
+angular.module('battle-ship-app').config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) => new Application.Config.RouteConfig($stateProvider, $urlRouterProvider)]);
 
 angular.module('battle-ship-app').controller('MainPageController', ['$scope', ($scope) => new Application.Controllers.MainPageController($scope)]);
-
-angular.module('battle-ship-app').config(['$stateProvider', ($stateProvider) => new Application.Config.RouteConfig($stateProvider)]);
